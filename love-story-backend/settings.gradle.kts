ccfc110659cfb9bca5f66eed2a54f5f0ed321f38
egglogic <lolita.s622@gmail.com>
2025-07-23 03:59:17 +0200

    "Build: Configure Gradle Foojay toolchain repository"

--- love-story-backend/settings.gradle.kts
+++ love-story-backend/settings.gradle.kts
@@ -1,1 +1,10 @@
 rootProject.name = "love-story-backend"
+toolchainManagement {
+    jvm {
+        javaRepositories {
+            repository("foojay") {
+                resolverClass.set(org.gradle.jvm.toolchain.foojay.FoojayToolchainResolver::class.java)
+            }
+        }
+    }
+}
