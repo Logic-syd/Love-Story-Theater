9fc8aaab983c18ae986300d73aee6eddda1969a1
egglogic <lolita.s622@gmail.com>
2025-07-23 04:08:24 +0200

    the last

--- love-story-backend/settings.gradle.kts
+++ love-story-backend/settings.gradle.kts
@@ -3,13 +3,3 @@ plugins {
 }
 
 rootProject.name = "love-story-backend"
-
-toolchainManagement {
-    jvm {
-        javaRepositories {
-            repository("foojay") {
-                resolverClass.set(org.gradle.jvm.toolchain.foojay.FoojayToolchainResolver::class.java)
-            }
-        }
-    }
-}
